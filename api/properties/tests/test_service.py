01264dbc7fa2986cb1cefa8edef2cccb30b34808
Jerry Mindek <jerry@Mindeks-MacBook-Air.local>
2020-10-25 22:19:30 -0400

    Stub api test file.
    Time prevents me from completing.

---
+++ api/properties/tests/test_service.py
@@ -0,0 +1,13 @@
+import pytest
+
+@pytest.mark.skip
+def test_set_selected(client):
+    pass
+
+@pytest.mark.skip
+def test_unset_selected(client):
+    pass
+
+@pytest.mark.skip
+def test_get_selected(client):
+    pass
